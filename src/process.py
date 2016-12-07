e1b1df45746682863651d8fabfe4114b5d09d48b
Dryden Bouamalay <bouamalayd@gmail.com>
2016-12-07 12:27:03 -0600

    processing now creates bidirectional hashes

--- src/process.py
+++ src/process.py
@@ -3,6 +3,15 @@ import enchant
 
 from data_parser import parser
 
+def flatten(lst):
+
+	"""
+	Flattens a list. Takes a list of lists such as [ [[],[],[]], [[],[],[]] ]
+	and returns [[], [], [], [], [], []]
+	"""
+
+	return [item for sublist in lst for item in sublist]
+
 def get_data(filename):
 	
 	"""
@@ -30,18 +39,39 @@ def filter_data(data):
 		data[idx] = map(lambda l: [w for w in l if en_dict.check(w)], d)
 	return data
 
-def flatten_data(data):
+def get_bidirectional(data):
 
 	"""
-	Take the list of poems containing lists of lines and flatten it such that
-	we only have a list of lines (one list of every line for all poems). 
+	From the flattened list of filtered data, create two hashes, one that maps 
+	a word to an id and one that maps the id back to the word. 
+	"""
+	
+	# flatten the flattened data once again to get a list of all tokens
+	tokens = flatten(data)
+
+	# generate the hashes
+	unique_tokens = set(tokens)
+	id_to_token = dict(enumerate(unique_tokens))
+	token_to_id = {v : k for k,v in id_to_token.iteritems()}
+
+	return id_to_token, token_to_id
+
+def make_indexed(data):
+
+	"""
+	Creates the training samples for the Baum-Welch algorithm. 
 	"""
 
-	return [line for poem in data for line in poem]
+	pass
 
 result = get_data('./data/raw/shakespeare.txt')
 filtered = filter_data(result)
-flattened = flatten_data(filtered)
+flattened = flatten(filtered)
 
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(flattened)
+print len(flattened)
+
+id_to_token, token_to_id = get_bidirectional(flattened)
+pp.pprint(id_to_token)
+pp.pprint(token_to_id)
